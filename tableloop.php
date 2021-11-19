c7361f9a817d2605fef217922d5e907839f419b1
pongbunnarak <89377972+pongbunnarak@users.noreply.github.com>
2021-11-19 15:52:56 +0700

    Create tableloop.php

---
+++ tableloop.php
@@ -0,0 +1,21 @@
+<html>
+  <head>
+  </head>
+  <body>
+    <table border="1">
+       <?php
+      for($i=0;$i<100;$i++){
+          echo "<tr>";
+          echo "<td>คอลัมน์1</td>";
+          echo "<td>คอลัมน์2</td>";
+          echo "<td>คอลัมน์3</td>";
+          echo "<td>คอลัมน์4</td>";
+          echo "<td>คอลัมน์5</td>";
+          echo "<td>คอลัมน์6</td>";
+          
+          echo "</tr>";
+        }
+       ?>
+    </table>
+  </body>
+</html>
